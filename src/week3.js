7cf65905b968a3361d5005ad4d3e6a953a7b976b
Heccin <Ree>
2021-03-09 10:36:29 +1300

    doin a variable

--- src/week3.js
+++ src/week3.js
@@ -1,1 +1,11 @@
 console.log('Reeeeeeeeee') 
+// TODO JavaScript Variables
+//variable is container. declare a variable with var 
+//a variable containing a value (name, number, string, function)
+var firstName = 'Nicole' 
+var lastName = 'Barnett'
+//declaring structure var, name, assing value with =
+//firstName is like the logo on a shipping conatiner
+console.log(firstName)
+//console.log is printing it on screen
+console.log(lastName)
