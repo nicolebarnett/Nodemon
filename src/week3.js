3b0fe7a93faac8dc81d2bd5eacecdf3c0696fce2
Heccin <Ree>
2021-03-09 10:52:51 +1300

    names

--- src/week3.js
+++ src/week3.js
@@ -3,9 +3,20 @@ console.log('Reeeeeeeeee')
 //variable is container. declare a variable with var 
 //a variable containing a value (name, number, string, function)
 var firstName = 'Nicole' 
-var lastName = 'Barnett'
+// var lastName = 'Barnett'
 //declaring structure var, name, assing value with =
 //firstName is like the logo on a shipping conatiner
 console.log(firstName)
 //console.log is printing it on screen
+
+// 3 js variables const, var, and let
+let lastName = 'Barnett'
 console.log(lastName)
+//let is more flexible, can have 2 of the same values
+//use let to reassign values, manipulate data, change value and different types of data
+//const is used to retain variable, it cannot be changed unless you delete let
+//to check code is behaving properly, console log to check for errors
+// to combine first name and last name with the operator + 
+let fullName = firstName + ' ' + lastName
+fullName = `My full name is ${firstName} ${lastName}`
+console.log(fullName)
